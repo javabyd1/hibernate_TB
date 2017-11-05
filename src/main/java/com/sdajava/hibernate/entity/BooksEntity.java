31d850d775d051b77f7571f5ea5fbc96029172fd
Tomek <java-tomek@users.noreply.github.com>
2017-11-05 12:34:04 +0100

    new books entity

--- src/main/java/com/sdajava/hibernate/entity/BooksEntity.java
+++ src/main/java/com/sdajava/hibernate/entity/BooksEntity.java
@@ -1,4 +1,46 @@
 package com.sdajava.hibernate.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.sql.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "books")
 public class BooksEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column
+    private int id;
+    @Column
+    private String title;
+    @Column
+    private String author;
+    @Column
+    private Date published;
+    @Column
+    private String isbn;
+    @Column
+    private String category;
+    @Column
+    private Integer pageCount;
+    @Column
+    private String publisher;
+    @Column
+    private BigDecimal price;
+    @Column(name = "on_stock")
+    private Integer onStock;
 }
